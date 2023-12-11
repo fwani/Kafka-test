d8644604a59575cbda9da79d2fc9548b7d41f4f8
fwani <seungfwani@gmail.com>
2023-12-11 16:55:21 +0900

    update spark example

--- mykafka-python/with_spark.py
+++ mykafka-python/with_spark.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 
 if __name__ == '__main__':
     spark = SparkSession.builder \
@@ -6,19 +7,28 @@ if __name__ == '__main__':
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
         .getOrCreate()
     kafka_bootstrap_servers = '0.0.0.0:9092,0.0.0.0:9093'
-    topic = 'spark1'
+    topic = 'fwani,spark1'
+
     df = spark \
         .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
         .option("subscribe", topic) \
+        .option("startingOffsets", "earliest") \
         .load()
 
-    ds = df \
+    df.groupby('topic').agg(F.count('value').alias('value')) \
         .writeStream \
+        .outputMode("update") \
         .format("console") \
-        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-        .option("subscribe", topic) \
-        .start()
+        .start() \
+        .awaitTermination()
 
-    ds.awaitTermination()
+    # df.groupby('topic').agg(F.count('value').cast('string').alias('value')) \
+    #     .writeStream \
+    #     .outputMode("update") \
+    #     .format("kafka") \
+    #     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    #     .option("checkpointLocation", "/tmp/vaquarkhan/checkpoint") \
+    #     .start() \
+    #     .awaitTermination()
